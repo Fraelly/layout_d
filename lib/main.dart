051d146432eff3000e903db56e85f3393c4234fc
Fraelly <fraelly@live.com>
2023-07-27 23:03:04 -0300

    pri1

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:namer_app/star.dart';
+import 'package:namer_app/text.dart';
 
-void main() => runApp(MeuMaterialApp());
 
-class MeuMaterialApp extends StatelessWidget {
+Future<void> main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('App Bar'),
-        ),
-        drawer: const Drawer(),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              label: 'Settings',
-            )
-          ],
-        ),
+      theme: ThemeData(
+        // useMaterial3: true,
+        primaryColor: Colors.lime
       ),
+      home: Column(
+        children: [
+          // Textt(),
+          Star(),
+        ],
+      )        
+                
     );
   }
 }
+
